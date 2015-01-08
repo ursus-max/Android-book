b335c51a2fee8720d960ef1527a43eed5fcfe1ec
Max Nesk <qamaxim@gmail.com>
2015-01-08 16:03:33 +0200

    New working activity

---
+++ CriminalIntent/app/src/main/java/kobza/ua/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,73 @@
+package kobza.ua.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by Max on 08.01.2015.
+ */
+public class CrimePagerActivity  extends FragmentActivity{
+    private ViewPager mViewPager;
+    private ArrayList <Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int pos) {
+                Crime crime = mCrimes.get(pos);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+        UUID crimeId = (UUID)getIntent()
+        .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        for (int a = 0; a< mCrimes.size(); a++){
+            if(mCrimes.get(a).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(a);
+                break;
+            }
+        }
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                Crime crime = mCrimes.get(i);
+                if (crime.getTitle()!=null){
+                    setTitle(crime.getTitle());
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+    }
+}
